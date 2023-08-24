778ec3c6a5693cd6c094008cff6cd19c29ca93df
campusricardo <campusricardo2023@gmail.com>
2023-08-24 07:49:15 -0500

    yeiiii

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -2,10 +2,17 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
 
+  const expenses = [
+    { id: 'e1', title: ' Car Insurance ', amount: 294.67, date: new Date(2021,3,10)},
+    {id: 'e2', title: ' Car Insurance2 ', amount: 80, date: new Date(2020,12,9)},
+    {id: 'e3', title: ' Car Insurance3 ', amount: 294.67, date: new Date(2021,11,10)},
+  ]
 
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem title={expenses[0].title}></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
