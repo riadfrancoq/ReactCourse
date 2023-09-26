3bad4746ece80c82b12423e003e4ca6dd3cab187
Sergio-Prada-963 <alejanprada140705@gmail.com>
2023-09-26 07:43:37 -0500

    feat: :sparkles: 1:01:30
    i am in the video number 5 in 1:01:30

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Expenses from './components/Expenses/Expenses.js';
-const App = ()=>  {
+import React from "react";
+import Expenses from "./components/Expenses/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense.js";
 
-  const expenses = [
-    { id: 'e1', title: ' Car Insurance ', amount: 294.67, date: new Date(2021, 3, 10) },
-    { id: 'e2', title: ' Car Insurance2 ', amount: 80, date: new Date(2020, 12, 9) },
-    { id: 'e3', title: ' Car Insurance3 ', amount: 294.67, date: new Date(2021, 11, 10) },
-  ]
+const App = () => {
+	const expenses = [
+		{ id: "e1", title: " Car Insurance ", amount: 294.67, date: new Date(2021, 3, 10) },
+		{ id: "e2", title: " Car Insurance2 ", amount: 80, date: new Date(2020, 12, 9) },
+		{ id: "e3", title: " Car Insurance3 ", amount: 294.67, date: new Date(2021, 11, 10) },
+	];
 
-  return (
-    <div>
-      <h2> Let's get started!</h2>
-      <Expenses items={expenses} />
-    </div>
-  );
-}
+	return (
+		<div>
+			<NewExpense />
+			<Expenses items={expenses} />
+		</div>
+	);
+};
 
 export default App;
-
--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,27 @@
-import ExpenseDate from './ExpenseDate.js';
-import Card from '../UI/Card.js'
-import './ExpenseItem.css';
-import React, {useState} from 'react';
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "../UI/Card.js";
+import "./ExpenseItem.css";
+import React, { useState } from "react";
 
-const ExpenseItem = ({date,title,amount}) => {
-    // no nested functions
-    const [useTitle, setTitle] = useState(title);
-    const clickHandler = () => {
-        setTitle('Updated');
-    };
+const ExpenseItem = ({ date, title, amount }) => {
+	// no nested functions
+	const [useTitle, setTitle] = useState(title);
+	console.log("ExpenseItem evaluated by React");
+	const clickHandler = () => {
+		setTitle("Updated");
+	};
 
-    // show  end with Handler
+	// show  end with Handler
 
-    return (
-        <Card className = "expense-item">
-            <ExpenseDate date={date}></ExpenseDate>
-            <div className= "expense-item__description"> 
-            <h2>{useTitle}</h2>
-            <div className= "expense-item__price">${amount}</div>
-            </div>
-            <button onClick={clickHandler}> Change Title</button>
-        </Card>
-    );
-}
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={date}></ExpenseDate>
+			<div className="expense-item__description">
+				<h2>{useTitle}</h2>
+				<div className="expense-item__price">${amount}</div>
+			</div>
+			<button onClick={clickHandler}> Change Title</button>
+		</Card>
+	);
+};
 export default ExpenseItem;
